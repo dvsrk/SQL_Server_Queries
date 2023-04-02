de14b65f598867ff2b9250566bc8a1249b133286
Siva Dasari <sdasari@surveymonkey.com>
2023-04-02 11:38:14 -0700

    test

---
+++ tes.sql
@@ -0,0 +1,232 @@
+﻿IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 31)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (31, 130, N'Nom', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 35)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (35, 130, N'Pangalan', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 40)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (40, 130, N'Nafn', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 13)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (13, 130, N'Nama', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 16)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (16, 130, N'Nome', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = الاسم)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (الاسم, 130, N'25', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 30)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (30, 130, N'Име', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 26)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (26, 130, N'Անուն', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 18)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (18, 130, N'Имя', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 29)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (29, 130, N'Ime', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 34)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (34, 130, N'Nimi', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 21)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (21, 130, N'Ad', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 54)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (54, 130, N'Jina', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 14)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (14, 130, N'Navn', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 4)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (4, 130, N'Navn', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 28)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (28, 130, N'নাম', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 17)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (17, 130, N'Nome', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 49)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (49, 130, N'ਨਾਮ', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 53)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (53, 130, N'Ime', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 19)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (19, 130, N'Nombre', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 39)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (39, 130, N'Név', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 2)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (2, 130, N'名字', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 32)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (32, 130, N'Ime', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 44)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (44, 130, N'Pilns vārds', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 6)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (6, 130, N'Nimi', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 22)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (22, 130, N'Ім’я', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 20)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (20, 130, N'Namn', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 55)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (55, 130, N'பெயர்', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 41)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (41, 130, N'Nama', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 27)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (27, 130, N'Izena', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 46)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (46, 130, N'Име', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 52)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (52, 130, N'Meno', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 45)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (45, 130, N'Vardas, pavardė', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 3)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (3, 130, N'名稱', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 24)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (24, 130, N'Emri', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 56)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (56, 130, N'పేరు', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 7)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (7, 130, N'Nom', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 33)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (33, 130, N'Jméno', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 8)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (8, 130, N'Name', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = שם)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (שם, 130, N'37', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 12)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (12, 130, N'이름', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 38)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (38, 130, N'नाम', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 47)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (47, 130, N'പേര്', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 42)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (42, 130, N'Ainm', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 10)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (10, 130, N'Nome', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 58)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (58, 130, N'Tên', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 50)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (50, 130, N'Nume', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 59)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (59, 130, N'Enw', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 11)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (11, 130, N'名前', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 36)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (36, 130, N'სრული სახელი', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = نام)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (نام, 130, N'48', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 19)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (19, 130, N'Nombre', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 5)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (5, 130, N'Naam', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 9)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (9, 130, N'Ονοματεπώνυμο', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 57)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (57, 130, N'ชื่อ-สกุล', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 51)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (51, 130, N'Ime', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 15)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (15, 130, N'Nazwa', getutcdate());
+END
+IF NOT EXISTS (SELECT 1 FROM test WHERE TextID = 130 AND LanguageId = 1)
+BEGIN
+INSERT INTO test (LanguageID, TextID, Text, Date_Modified) VALUES (1, 130, N'Name', getutcdate());
+END
